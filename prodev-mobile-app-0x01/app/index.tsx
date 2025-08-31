46f05a5ee6e983651a43863f7e7776585cc4529a
Ann <annkarokiadmissions@gmail.com>
2025-08-31 20:11:58 +0300

    fix error

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,9 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function EntryScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
+      <Text>Entry Screen - Awesome</Text>
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
     </View>
   );
 }
